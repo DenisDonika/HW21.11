d1e018b0ebb31b447fe74ac2324f685ce899a137
Denis <balance.donika@gmail.com>
2022-12-01 14:06:15 +0600

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input first numbers");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second numbers");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+}
+else
+{
+    Console.WriteLine($"max = {number2}");
+}
